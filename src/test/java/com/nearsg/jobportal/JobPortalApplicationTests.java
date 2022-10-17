86916b6f882a2165272ab16ec364571abd012a38
Csaba Soti <sotcsa@gmail.com>
2022-10-17 06:48:34 +0200

    Add docker

--- src/test/java/com/nearsg/jobportal/JobPortalApplicationTests.java
+++ src/test/java/com/nearsg/jobportal/JobPortalApplicationTests.java
@@ -2,12 +2,12 @@ package com.nearsg.jobportal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class JobPortalApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
